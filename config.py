ca9078ba3a16a79af88a2548cdb19ca093d7c3bf
Abhay <77850911+Abhay056@users.noreply.github.com>
2023-05-31 00:38:43 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+que = {}
+admins = {}
+
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN","")
+BOT_NAME = getenv("BOT_NAME","Python Code Executer")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "120"))
+SESSION_NAME = getenv("SESSION_NAME", "")
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
